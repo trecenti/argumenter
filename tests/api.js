5d53944aec79ed712bab9592b02e4ad8c872e91f
Bruno Trecenti <btrecent@thoughtworks.com>
2013-12-06 10:49:42 -0200

    added spreader for a given index

--- tests/api.js
+++ tests/api.js
@@ -1,15 +1,47 @@
 var argumenter = require('../index');
 
 describe('the argumenter api', function () {
-  it('can be called in a chain', function () {
-    function fn() {
-      return argumenter(fn)
-        .when(1, function(one) { return one; })
-        .when(0, function() { return 'no-arguments'; })
-        .done();
-    };
+  describe('when chained', function () {
+    it('does not throw', function () {
+      function fn() {
+        return argumenter(fn)
+          .when(1, function(one) { return one; })
+          .when(0, function() { return 'no-arguments'; })
+          .done();
+      };
 
-    expect(fn).to.not.throw();
+      expect(fn).to.not.throw();
+    });
+
+    it('does not throws (if no strategy is defined)', function () {
+      function fn() {
+        return argumenter(fn).done();
+      };
+
+      expect(fn).to.not.throw();
+    });
+
+    it('throws (when done is called in a wrong order)', function () {
+      function fn() {
+        return argumenter(fn)
+          .when(1, function(one) { return one; })
+          .done()
+          .when(0, function() { return 'no-arguments'; });
+      };
+
+      expect(fn).to.throw();
+    });
+
+    it('throws (when spread called in a wrong order)', function () {
+      function fn() {
+        return argumenter(fn)
+          .when(1, function(one) { return one; })
+          .spread(0)
+          .when(0, function() { return 'no-arguments'; });
+      };
+
+      expect(fn).to.throw();
+    });
   });
 
   it('can be called detached', function () {
@@ -17,6 +49,7 @@ describe('the argumenter api', function () {
       var handler = argumenter(fn);
 
       handler.when(1, function(one) { return one; });
+      handler.spread(0);
       handler.when(0, function() { return 'no-arguments'; });
 
       return handler.done();
@@ -24,5 +57,4 @@ describe('the argumenter api', function () {
 
     expect(fn).to.not.throw();
   });
-
 });
---
+++ tests/spreader.js
@@ -0,0 +1,57 @@
+var argumenter = require('../index');
+
+describe('spreader', function () {
+  it('spread array argument on a give index', function () {
+    function fn() {
+      var handler = argumenter(fn);
+
+      handler.spread(0).spread(2)
+        .when([String, Object, Number, Array], function (string, obj, n, array) {
+          return {
+            string: string,
+            obj: obj,
+            number: n,
+            array: array
+          };
+        });
+
+      return handler.done();
+    };
+
+    expect(
+      fn(['test', { a: 1 }], 1, [[3,4]])
+    ).to.deep.equal({
+      string: 'test',
+      obj: { a: 1 },
+      number: 1,
+      array: [3, 4]
+    });
+  });
+
+  it('does not throw if arg to spread is not an array', function () {
+    function fn() {
+      var handler = argumenter(fn);
+
+      handler.spread(0).spread(1).spread(2);
+
+      handler
+        .when([String, Object, Number, Array], function (string, obj, n, array) {
+          return {
+            string: string,
+            obj: obj,
+            number: n,
+            array: array
+          };
+        });
+
+      return handler.done();
+    };
+
+    expect(fn(['test', { a: 1 }], 1, [[3,4]])).to.deep.equal({
+      string: 'test',
+      obj: { a: 1 },
+      number: 1,
+      array: [3, 4]
+    });
+  });
+});
